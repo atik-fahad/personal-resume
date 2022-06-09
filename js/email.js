87d286bb33a1987b6d3e6f04765d2b6906e05ad9
Atik Fahad <fahadatik@gmail.com>
2022-06-09 17:31:12 +0600

    initial form submit

---
+++ js/email.js
@@ -0,0 +1,22 @@
+document.querySelector('button[type=submit]').addEventListener('click', (e) => {
+    e.preventDefault();
+    let name = document.querySelector('#name').value;
+    let email = document.querySelector('#email').value;
+    let description = document.querySelector('#description').value;
+    let data = { name, email, description };
+    console.log(JSON.stringify(data))
+    fetch('../contact-form.php', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Success:', data);
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    });
+})
